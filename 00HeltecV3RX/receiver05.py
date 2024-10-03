373330fdf9be0ccbe03c07d06646f0928041f0f6
m2mlorawan <38450747+m2mlorawan@users.noreply.github.com>
2024-10-03 12:08:12 +0700

    Update receiver05.py

--- 00HeltecV3RX/receiver05.py
+++ 00HeltecV3RX/receiver05.py
@@ -8,7 +8,7 @@ pin21.value(1) #heltec V3
 
 # init ic2 object
 i2c = machine.SoftI2C(scl=machine.Pin(18), sda=machine.Pin(17), freq=20000) #heltec V3
-lcd=ssd1306.SSD1306_I2C(128,64,i2c,60)
+oled=ssd1306.SSD1306_I2C(128,64,i2c,60)
 
 sx = SX1262(spi_bus=1, clk=9, mosi=10, miso=11, cs=8, irq=14, rst=12, gpio=13)
 # LoRa
@@ -19,19 +19,19 @@ sx.begin(freq=923, bw=125.0, sf=12, cr=5, syncWord=0x12,
          tcxoVoltage=1.7, useRegulatorLDO=False, blocking=True)
 
 
-lcd.text("Start listening",0,0)
-lcd.show()
+oled.text("Start listening",0,0)
+oled.show()
 time.sleep(5)
 
 while True:
     msg, err = sx.recv()
     if len(msg) > 0:
-        lcd.fill(0)
-        lcd.show()
+        oled.fill(0)
+        oled.show()
         error = SX1262.STATUS[err]
         print(msg)
         print(error)
-        lcd.text(msg,0,0)
-        lcd.show()
+        oled.text(msg,0,0)
+        oled.show()
         time.sleep(5)
 
